8c6243014fbe6071321cfd2c8734c2931a868738
SuperiorDingus <60527656+SuperiorDingus@users.noreply.github.com>
2020-04-08 08:59:01 -0500

    Update display.js

--- js/display.js
+++ js/display.js
@@ -2,14 +2,20 @@
 
 const Display = function(){
     Display.render = function(){
+        //clears sprite layer
         sctx.clearRect(0,0, sprites.width, sprites.height);
         sctx.save();
-        sctx.translate(player1.x,  player1.y);
+        
+        // Moves layer so player is flippable. Rounds so it doesn't look fuzzy.
+        sctx.translate(Math.round(player1.x - camera.x),  Math.round(player1.y - camera.y));
         sctx.scale(1 * player1.direction,1);
-        sctx.drawImage(Display.getAnimation(),-22, -35,44,70);
+        sctx.drawImage(Display.getAnimation(),-22, -36,44,70);
         sctx.restore();
-        sctx.fillRect(0,430,2000,1000);
-
+        
+        //Hitbox
+        //sctx.fillRect(player1.x-16,player1.y-30,32,60);
+        
+        Display.drawMap();
     };
     Display.getAnimation = function(){
         frameNumber++;
@@ -18,5 +24,26 @@ const Display = function(){
         }
         davidFrame = Math.floor(frameNumber/25 + 1);
         return document.getElementById("idle"+davidFrame);
+    };
+    Display.drawMap = function(){
+        mctx.save();
+        mctx.translate(-camera.x, -camera.y);
+        for (var i = 0; i < map.length; i += 3){
+            if (map[i] == 1){
+                mctx.fillStyle = "black";
+            }
+            if (map[i] == 2){
+                mctx.fillStyle = "green";
+            }
+            if (map[i] == 3){
+                mctx.fillStyle = "yellow";
+            }
+            mctx.fillRect(map[i+1]*35,map[i+2]*35,35,35);
+        }
+        mctx.restore();
+    };
+    
+    Display.refresh = function(){
+        mctx.clearRect(0,0, sprites.width, sprites.height);
     };
 };
